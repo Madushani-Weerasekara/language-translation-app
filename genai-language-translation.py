256f8dd3993746a82270fe642b1871304d60d5fd
Madushani-Weerasekara <mmweerasekara@gmail.com>
2025-04-23 15:46:05 +0530

    Language Translator App Completed

--- genai-language-translation.py
+++ genai-language-translation.py
@@ -2,22 +2,22 @@ import getpass
 import os
 import streamlit as st
 from dotenv import load_dotenv
-from langchain_core. output_parsers import StrOutputParser
+from langchain_core.output_parsers import StrOutputParser
 
 load_dotenv()
 
-os.environ["GOOGLE_API_KEY"] = os.getpass("Enter your google AI API key : ")
+#os.environ["GOOGLE_API_KEY"] = getpass.getpass("Enter your google AI API key : ")
 
 
 os.environ["GOOGLE_API_KEY"] = os.getenv("GOOGLE_API_KEY")
 os.environ["LANGCHAIN_TRACING_V2"] = "true" # To start the tracing make it true
-os.environ["LANGCHAIN-API-KEY"] = os.getenv("LANGCHAIN_API_KEY")
+os.environ["LANGCHAIN_API_KEY"] = os.getenv("LANGCHAIN_API_KEY")
 
 # Now we can instantiate our generate chat completions
 from langchain_google_genai import ChatGoogleGenerativeAI
 
 llm = ChatGoogleGenerativeAI(
-    model = "gemini - 1.5-pro",
+    model = "gemini-1.5-pro",
     temperature=0,
     max_tokens=None,
     timeout=None,
@@ -26,9 +26,9 @@ llm = ChatGoogleGenerativeAI(
 )
 
 # We can chain our model with a prompt template like so:
-from langchain_core.prompts import ChatMessagePromptTemplate
+from langchain_core.prompts import ChatPromptTemplate
 
-prompt = ChatMessagePromptTemplate.format_messages(
+prompt = ChatPromptTemplate.from_messages(
     [
         (
 
@@ -40,8 +40,8 @@ prompt = ChatMessagePromptTemplate.format_messages(
     ]
 )
 
-st.title("Langchain Demo with Gemini(Language Translation")
-input_text = st.text_input("Write the sentence in Englidh and it will be translated")
+st.title("Langchain Demo with Gemini(Language Translator")
+input_text = st.text_input("Write the sentence in English and it will be translated")
 
 output_parser = StrOutputParser()
 
@@ -51,8 +51,8 @@ if input_text:
     st.write(chain.invoke(
         {
             "input_language" : "English",
-            "output_language" : "German",
-            "input_text": input_text,
+            "output_language" : "Sinhala",
+            "input": input_text,
             
         }
     ))
